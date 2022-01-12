4bf61ca2d44940682450b83deda060ef829c1d00
danielgitk <danielk1781@gmail.com>
2022-01-12 13:42:24 +0300

    another one

---
+++ branches/target/debug/deps/branches-619c033c54e2f7f8.d
@@ -0,0 +1,5 @@
+/home/daniel/Documents/dev/R/branches/target/debug/deps/branches-619c033c54e2f7f8.rmeta: src/main.rs
+
+/home/daniel/Documents/dev/R/branches/target/debug/deps/branches-619c033c54e2f7f8.d: src/main.rs
+
+src/main.rs:
---
+++ branches/target/debug/deps/branches-c03d9f47910bf5dd.d
@@ -0,0 +1,5 @@
+/home/daniel/Documents/dev/R/branches/target/debug/deps/branches-c03d9f47910bf5dd.rmeta: src/main.rs
+
+/home/daniel/Documents/dev/R/branches/target/debug/deps/branches-c03d9f47910bf5dd.d: src/main.rs
+
+src/main.rs:
